cbe0f75c7586e0a551e900a8455b321a8872fcac
Alvaro Mercado <alvaroawake@gmail.com>
2015-02-21 16:52:47 -0400

    first commit

---
+++ app/src/main/java/com/example/secretara_santuario/telefonodelpadre/LlamadaActivity.java
@@ -0,0 +1,15 @@
+package com.example.secretara_santuario.telefonodelpadre;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LlamadaActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llamada);
+
+    }
+}
---
+++ app/src/main/java/com/example/secretara_santuario/telefonodelpadre/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.secretara_santuario.telefonodelpadre;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    Button btnLlamar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLlamar=(Button)findViewById(R.id.btnLlamar);
+        btnLlamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent llamar=new Intent(MainActivity.this, LlamadaActivity.class);
+                startActivity(llamar);
+            }
+        });
+}
+}
